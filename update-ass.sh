c10b053374d4efe7f3dea0cec50024b0914ec204
anoreggit01 <88856585+anoreggit01@users.noreply.github.com>
2021-10-31 20:41:32 -0300

    Update update-ass.sh

--- update-ass.sh
+++ update-ass.sh
@@ -1,1 +1,6 @@
-0 6 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null > update-ass.sh
+0 6 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
+0 9 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
+0 10 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null 
+0 14 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null 
+0 15 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
+0 18 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
