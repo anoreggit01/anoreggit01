406973146ea442a156007beff26fe17f1a920e42
anoreggit01 <88856585+anoreggit01@users.noreply.github.com>
2021-11-03 16:39:33 -0300

    Update update-ass6.sh

--- update-ass6.sh
+++ update-ass6.sh
@@ -1,2 +1,2 @@
-0 6 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
+0 9 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
 
