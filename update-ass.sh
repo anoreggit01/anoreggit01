ab6c7f0a99afb037ef2d1e7187802df288a3382f
anoreggit01 <88856585+anoreggit01@users.noreply.github.com>
2021-11-03 10:55:38 -0300

    Delete update-ass.sh

--- update-ass.sh
+++
@@ -1,6 +0,0 @@
-0 6 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
-0 9 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
-0 10 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null 
-0 14 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null 
-0 15 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
-0 18 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
