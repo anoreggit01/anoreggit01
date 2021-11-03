d1c5117258806f46530ae97f5d2f6b6d7bd6241f
anoreggit01 <88856585+anoreggit01@users.noreply.github.com>
2021-11-03 16:40:30 -0300

    Update update-ass10.sh

--- update-ass10.sh
+++ update-ass10.sh
@@ -1,1 +1,1 @@
-0 10 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null 
+0 13 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null 
