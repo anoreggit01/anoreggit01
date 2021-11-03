84e63d74e18c04549bb1c88427be0d0c4d04f4eb
anoreggit01 <88856585+anoreggit01@users.noreply.github.com>
2021-11-03 16:39:49 -0300

    Update update-ass9.sh

--- update-ass9.sh
+++ update-ass9.sh
@@ -1,1 +1,1 @@
-0 9 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
+0 12 * * * /usr/bin/curl --silent http://localhost:80/atualizacao_associados.php &>/dev/null
